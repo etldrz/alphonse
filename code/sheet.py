bd2883b2e37fa50a48321c82d133b776a6673500
Evan Zook <evanzook@vt.edu>
2023-09-25 11:21:55 -0400

    Further command updating for \!sheet (my eyes are bleeding)

--- code/sheet.py
+++ code/sheet.py
@@ -24,7 +24,7 @@ parent = '1mGKri2dKu7E28BrN9nVMtU6qBszTt7QC' #Parent file for Google Sheet relat
 
 drive = build('drive', 'v3', credentials=credentials)
 
-curr_sheet = None # holds the name of the current working sheet
+curr_sheet = "test" # holds the name of the current working sheet
 
 affirmative = '\U0001F44D' #Al's reaction to a message when the job is completed successfully.
 
@@ -44,31 +44,86 @@ async def dm_error():
 
 
 class SheetGet:
+    
+    """
+    FORMATTING: get 'a/'i/curr/SHEET_NAME
+    IF LAST TWO THEN SHEET EMBED LINKED IS OUTPUTTED
+    ELSE:
+        'r/'p
+    IF RECENT, THEN RECENT DATA OUTPUTTED AS TEXT
+    IF PLOT, THEN:
+        pie/bar
+    FOR ATTENDANCE AND INVENTORY, THE SHEET NAME WILL BE SPECIFIED AT THE END.
+    """
 
-    async def eval_next(self, bot, ctx, text):
+    specific_sheet = [["attendance", "'a"], ["inventory", "'i"]]    
+    data_formatting = [["recent", "'r"], ["plot", "'p"]]
+    plot_types = ["pie", "bar"]
+    
+              
+    async def eval_next(self, ctx, text):
         if len(text) == 0:
             await ctx.send("Please further speciffy what you want.")
             return
 
         text.pop(0)
         if text[0] == "list":
-            await self.list(bot, ctx)
+            await self.list(ctx)
             return
-    
+        elif text[0] in inv_comms:
+            await self.inventory(ctx, text)
+            return
+        elif text[0] in att_comms:
+            await self.attendance(ctx, text)
+            return
+        
         exists = None
         if text[0] == "curr":
             exists = find(curr_sheet)
             text = curr_sheet
         else:
             exists = find(" ".join(text))
+        
         if exists == None:
-            await ctx.send("The specified file does not exist.")
+            await ctx.send("Bad command.")
             return
 
-        await self.get(bot, ctx, text, exists)
+        await self.get_link(ctx, text, exists)
+
+    
+    async def get_data(self, ctx, spreadsheet_id, pull_range):
+        try:
+            service = build('sheets', 'v4', credentials=credentials)
+            result = service.spreadsheets().values().get(
+                spreadsheetId=spreadsheet_id, range="Attendance!A1:E" + pull_length
+            ).execute()
+
+        except HttpError as err:
+            await dm_error(ctx)
+            return
+                
+        data = result.get('values', [])
+        await ctx.send(len(data)) #check to see if len(list) where list is a list containing list has dimension just equal to no. of lists.
+        return data
 
 
-    async def list(self, bot, ctx):
+    async def attendance(self, ctx, text):
+        #ignoring recent and plot for now
+
+        pull_length = 150
+        range = "Attendance!A1:E" + pull_length
+        data = await self.get_data(ctx, spreadsheet_id, range)
+        
+        if len(data) == pull_length:
+            await ctx.send("Please switch to a new sheet for future data, this one has too many rows.")
+            pull_length = pull_length * 3
+            range = "Attendance!A1:E" + pull_length
+            data = await self.get_data(ctx, spreadsheet_id, range)
+        
+        await ctx.send(data)
+
+
+    async def list(self, ctx):
         sheet_list = drive.files().list(q= f"'{parent}' in parents and trashed=False", orderBy='recency').execute()
 
         message = "VTFC data sheets:\n"
@@ -79,7 +134,7 @@ class SheetGet:
         await ctx.send(message)
 
 
-    async def get(self, bot, ctx, text, exists):
+    async def get_link(self, ctx, text, exists):
         spreadsheet_id = exists['id']
         url = f'https://docs.google.com/spreadsheets/d/{spreadsheet_id}'
         embed = discord.Embed()
@@ -122,17 +177,118 @@ class SheetSet:
 
         if len(text) == 0:
             await ctx.send("Please specify what you would like to set.")
-        
+            return
+
+        if text[0] == "curr":
+            await self.curr(ctx, text)
+            return
+        elif text[0] == "value":
+            await self.value(ctx, text)
+            return
+        await ctx.send("Bad command")
     
-    async def attendance():
+
+    async def value(self, ctx, text):
+        text.pop(0)
+        if len(text) == 0:
+            await ctx.send("Bad command")
+            return
+
+        if text[0] == "attendance" or text[0] == "'a":
+            await self.attendance(ctx, text)
+            return
+        elif text[0] == "inventory" or text[0] == "'i":
+            await self.inventory(ctx, text)
+            return
+        await ctx.send("Bad command")
+
+
+    async def attendance(self, ctx, text):
+        text.pop(0)
+        if len(text) == 0:
+            await ctx.send("Bad command")
+            return
+
+        if len(text) != 6:
+            await ctx.send("Please specify the details for all three weapons in the format"\
+                           " [AMOUNT WEAPON_NAME]")
+            return
+
+         curr_date = None
+         if text[0] == "today":
+             curr_date = date.today()
+
+        values = None
+        if all(map(isnumeric, text[1, 3, 5])):
+            values = map(int, text[1, 3, 5])
+        else:
+            await ctx.send("Please specify the data formatting as [AMOUNT WEAPON_NAME].")
+        
+        check = all(i in text[2, 4, 6] for w in ["epee", "foil", "sabre"])
+
+        if not check:
+            await ctx.send("Please specify the weapon names more better")
+            return
+        
+        
+
+
+
+        async def inventory():
         return
 
-    async def inventory():
-        return
+        
+
+
+
+    if not check:
+            await ctx.send("Please specify the weapons more better.")
+            return
 
     async def curr():
         return
 
+
+class SheetRead: #DEFcunt
+    
+    async def eval_next(self, ctx, text):
+
+        
+
+        """
+        ADD FUNCTIONALITY FOR PLOTTING AND CAT (PROBABLY SET UP FOR RECENT ONLY).
+        MAKE IT SO THAT THE FUNCTIONS THAT PULL DATA DO IT THE SAME NO MATTER WHAT,
+        IT'S JUST THE FUNCTIONS FOR PLOTTING AND CATTING THAT ALTER IT.
+        """
+
+        text.pop(0)
+        if len(text) == 0:
+            await ctx.send("Please further specify commands")
+            return
+
+        if text[0] == "curr":
+            await self.curr(ctx, text)
+            return
+        elif text[0] == "attendance" or text[0] == "'a":
+            await self.armory(ctx, text)
+            return
+        elif text[0] == "inventory" or text[0] == "'i":
+            await self.inventory(ctx, text)
+            return
+
+        data = text.pop(len(text) - 1)
+        exists = None
+        if end == "attendance" or end == "'a" or end == "inventory" or end == "'i":
+            exists = find(" ".join(text))
+
+        if exists == None:
+            await ctx.send("Bad sheet name specification")
+            return
+
+        await self.specific(ctx, exists, data)
+
+
+        
 
 
 class SheetBuild:
